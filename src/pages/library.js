8ad7a2e06b430a54478e470e229c5a925307ad24
PixelNote <andresito_s2@live.com>
2023-11-29 12:44:58 -0500

    Add search bar

--- src/pages/library.js
+++ src/pages/library.js
@@ -1,36 +1,95 @@
-import React,  { useEffect } from 'react'
+import React,  { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/authContext';
+import { getBooks } from '../services/firebaseService';
 
 
-export default function Libray(){
+export default function Library(){
 
   const auth = useAuth();
   const navigate = useNavigate();
-  
-  useEffect(() => {
-    if(auth.user===""){
-      navigate('/')
 
-   }
-  }, [auth.user]);
+  const { email, displayName } = auth.user
+
+  const [books, setBooks] = useState([])
+  const [filteredBooks, setFilteredBooks] =  useState([])
+  const [inputSearch, setInputSearch]  = useState('')
+
+
+  useEffect(()=>{
+    const fetchBooks = async () => {
+      const fetchedBooks = await getBooks(email)
+      setBooks(fetchedBooks)
+    };
+    fetchBooks()
+    
+  },[])
+
+  useEffect(() => {
+    if (inputSearch.trim() === "") {
+      setFilteredBooks(books);
+      console.log(filteredBooks)
+    } else {
+      const result = books.filter((book) => {
+        return (
+          book.title.toLowerCase().includes(inputSearch.toLowerCase()) ||
+          book.author.toLowerCase().includes(inputSearch.toLowerCase())
+        );
+      });
+
+      setFilteredBooks(result);
+    }
+  }, [inputSearch, books]);
+
+  const filterList = (input) => {
+    setInputSearch(input.trim() === "" ? "" : input);
+  };
+  
+
 
   const handleLogout = () => {
     auth.logout();
   }
-  
 
   return (
     <div className="page">
-      <div className="library-buttons">
-        <Link to="/">
-          <button onClick={() => handleLogout()} className="library-logout">
-            Logout
-          </button>
-        </Link>
-        <Link to="/library/add">
-          <button className="library-addbook">Agregar un libro</button>
-        </Link>
+      <div className="library-page">
+        <div className="library-nav">
+          <p className="library-title">Bienvenido, {displayName}</p>
+
+          <input
+            type="text"
+            className="library-search"
+            value={inputSearch}
+            placeholder="Busca tu libro por titulo o autor"
+            onChange={(e) => {
+              filterList(e.target.value);
+            }}
+          ></input>
+
+          <div className="library-buttons">
+            <Link to="/library/add">
+              <button className="library-addbook">Agregar un libro</button>
+            </Link>
+            <Link to="/">
+              <button onClick={() => handleLogout()} className="library-logout">
+                Cerrar sesión
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="library-books">
+          {filteredBooks.map((element) => (
+            <div className="book" key={element.id}>
+              <img
+                className="book-image"
+                src={element.image}
+                alt={`imagen de ${element.title}`}
+              />
+              <p className="book-title">{element.title}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/services/firebaseService.js
+++ src/services/firebaseService.js
@@ -1,21 +1,16 @@
 import { storage } from "../firebase/config";
 import { db } from "../firebase/config"; 
-import { setDoc, collection, doc } from "firebase/firestore"
+import { setDoc, getDocs, collection, doc } from "firebase/firestore"
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-
 
 export async function saveBook(email, file, title, author, genre, date ){
 
+  try{
   const storageRef = ref(storage, `${email}/${title}`)
-  
   await uploadBytes(storageRef, file)
-
   const url = await getDownloadURL(storageRef)
-  
   const userDocRef = doc(db, 'users', `${email}`)
   const booksCollection = collection(userDocRef, 'books')
-
-
   await setDoc(doc(booksCollection, title), {
     'title': title,
     'image': url,
@@ -23,5 +18,25 @@ export async function saveBook(email, file, title, author, genre, date ){
     "genre": genre,
     'date': date,
   });
+  alert("Libro agregado con exito");
+  }catch(e){
+    console.log(e.message)
+  }
+}
+
+export async function getBooks(email){
+  try{
+    
+  const userDocRef = doc(db, "users", `${email}`);
+  const booksCollection = collection(userDocRef, "books");
+  const querySnapshot = await getDocs(booksCollection)
+  const response = []
+  querySnapshot.forEach((doc) => {
+    response.push(doc.data())
+  });
+  return response
+  }catch(e){
+    console.log(e.message)
+  }
 
 }
